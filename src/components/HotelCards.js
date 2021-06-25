d3ee521c3b75df6df1b538efb69e8b4f43f93488
juanhatum@gmail.com <juanhatum@gmail.com>
2021-06-25 01:07:07 -0500

    prueba hecha en react terminada

--- src/components/HotelCards.js
+++ src/components/HotelCards.js
@@ -12,10 +12,10 @@ import {
   Rating,
   AirbnbRating,
 } from "react-native-elements";
-import { useAssets } from "expo-asset";
+import LottieView from "lottie-react-native";
 
 const HotelCards = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [state, setState] = useState([]);
   useEffect(() => {
     axios.get("http://192.168.1.2:3000/hotels").then((resp) => {
@@ -30,12 +30,13 @@ const HotelCards = () => {
   };
 
   return (
-    <View>
+    <View style={{ paddingBottom: 300 }}>
       <Header
-        placement="right"
-        leftComponent={{ icon: "menu", color: "#fff" }}
-        centerComponent={{ text: "CHEIL", style: { color: "#fff" } }}
+        placement="center"
+        centerComponent={{ text: "CHEIL", style: { color: "#fff" }}}
+        rightComponent={{ icon: 'home' }}
       />
+      
       <SearchBar
         onChangeText={(val) => {
           handleSearch(val);
@@ -46,32 +47,38 @@ const HotelCards = () => {
 
       <ScrollView>
         <View>
-          <Text>{query}</Text>
-
-          {
-            state.filter((value) => {
-             if(value == ''){
-               return value;
-             } else if (value.name.toLowerCase().includes(query)){
-               return value;
-             }
+          {state
+            .filter((value) => {
+              if (value == "") {
+                return value;
+              } else if (value.name.toLowerCase().includes(query)) {
+                return value;
+              }
             })
-          .map((hotel, i) => {
-            return (
-              <View key={i}>
-                <Card>
-                  <Card.Title>{hotel.name}</Card.Title>
-                  <Card.Divider />
-                  <Image
-                    source={{ uri: hotel.img }}
-                    style={{ width: 500, height: 200 }}
-                  />
-                  <Text style={{fontWeight:'bold'}}>{hotel.description}</Text>
-                  <AirbnbRating size={25} reviews={['']} isDisabled={true} count={5} defaultRating={hotel.stars} />
-                </Card>
-              </View>
-            );
-          })}
+            .map((hotel, i) => {
+              return (
+                <View key={i}>
+                  <Card>
+                    <Card.Title>{hotel.name}</Card.Title>
+                    <Card.Divider />
+                    <Image
+                      source={{ uri: hotel.img }}
+                      style={{ width: 500, height: 200 }}
+                    />
+                    <Text style={{ fontWeight: "bold" }}>
+                      {hotel.description}
+                    </Text>
+                    <AirbnbRating
+                      size={25}
+                      reviews={[""]}
+                      isDisabled={true}
+                      count={5}
+                      defaultRating={hotel.stars}
+                    />
+                  </Card>
+                </View>
+              );
+            })}
         </View>
       </ScrollView>
     </View>
